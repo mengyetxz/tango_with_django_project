386a1c495dd2ccd3126c0b86a7572e319cdcd8f4
mengyetxz <mengyetxz@gmail.com>
2016-10-21 15:07:39 +0800

    add static media

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -92,8 +92,12 @@ STATIC_URL = '/static/'
 #add template directory
 
 TEMPLATE_DIRS = (
+
     # Put string here, like "/home/html/django_templates" or "C:/www?django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use abslute paths, not relative paths.
     TEMPLATE_PATH,
 )
+
+
+
