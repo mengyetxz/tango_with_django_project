dc2e0e2331b1790c0c8da4c194347694c24ad763
mengyetxz <mengyetxz@gmail.com>
2016-10-23 21:25:48 +0800

    add template

--- rango/views.py
+++ rango/views.py
@@ -10,7 +10,7 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    backToIndex = "<br/> <a href='/rango/'>Index</a>"
-    return HttpResponse("Rango says here is the about page %s" % backToIndex)
+    context_dict = {'boldmessage': "This Page is ..."}
+    return render(request, 'rango/about.html', context_dict)
 
 
